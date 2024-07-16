59b69e20d9ea95968f4f5cb583ec1e4e66920cf3
Tommaso Pagliari <tpagliari98@gmail.com>
2024-07-16 16:08:59 +0200

    Initial commit

---
+++ agenda.py
@@ -0,0 +1,48 @@
+class Event:
+    def __init__(self, title, date, description):
+
+    def __str__(self):
+
+
+class Agenda:
+    def __init__(self):
+
+
+    def add_event(self, title, date, description):
+
+    def remove_event(self, title):
+
+    def list_events(self):
+
+
+if __name__ == "__main__":
+    agenda = Agenda()
+    print(
+        """
+     _____      _ _           _                                _       
+    |  __ \    | (_)         (_)     /\                       | |      
+    | |__) |__ | |_ _ __ ___  _     /  \   __ _  ___ _ __   __| | __ _ 
+    |  ___/ _ \| | | '_ ` _ \| |   / /\ \ / _` |/ _ \ '_ \ / _` |/ _` |
+    | |  | (_) | | | | | | | | |  / ____ \ (_| |  __/ | | | (_| | (_| |
+    |_|   \___/|_|_|_| |_| |_|_| /_/    \_\__, |\___|_| |_|\__,_|\__,_|
+                                          __/ |                       
+                                         |___/                        
+    
+    Welcome to the Polimi Agenda Application!
+    """
+    )
+    while True:
+        print("\nMenu:")
+        print("1. Add Event")
+        print("2. Remove Event")
+        print("3. List Events")
+        print("4. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+        elif choice == "2":
+        elif choice == "3":
+        elif choice == "4":
+            break
+        else:
+            print("Invalid choice, please try again.")
---
+++ test_agenda.py
@@ -0,0 +1,17 @@
+import unittest
+from agenda import Agenda
+
+
+class TestAgenda(unittest.TestCase):
+    def setUp(self):
+        self.agenda = Agenda()
+
+    def test_add_event(self):
+
+    def test_remove_event(self):
+
+    def test_list_events(self):
+
+
+if __name__ == "__main__":
+    unittest.main()
